be110a17024d64557d4da2176a5e462f39341d83
Iris Helbling <iris@lunar-legion.local>
2020-12-01 11:44:32 -0500

    woops forgot these

---
+++ scripts/onUseMedHypo.js
@@ -0,0 +1,14 @@
+const _ = require('lodash')
+
+module.exports = function onUseMedHypo(game, helpers, item) {
+  const player = game.getPlayer()
+
+  // TODO this should be a game.healCreature function or something
+  player.hp += helpers.diceRoll(item.diceCount, item.diceSize) + item.bonus
+  if (player.hp > player.hpMax) {player.hp = player.hpMax}
+  player.ap -= item.apCostBase
+
+  // TODO this should belong to a game.removeEntity function or something. Lodash doesnt belong here.
+  player.inventory = _.without(player.inventory, item)
+  game.state.items = _.without(game.state.items, item)
+}
---
+++ scripts/onUseStimHypo.js
@@ -0,0 +1,14 @@
+const _ = require('lodash')
+
+module.exports = function onUseMedHypo(game, helpers, item) {
+  const player = game.getPlayer()
+
+  // TODO this should be a game.healCreature function or something
+  player.ap += helpers.diceRoll(item.diceCount, item.diceSize) + item.bonus
+  if (player.ap > player.apMax) {player.ap = player.apMax}
+  player.ap -= item.apCostBase
+
+  // TODO this should belong to a game.removeEntity function or something. Lodash doesnt belong here.
+  player.inventory = _.without(player.inventory, item)
+  game.state.items = _.without(game.state.items, item)
+}
